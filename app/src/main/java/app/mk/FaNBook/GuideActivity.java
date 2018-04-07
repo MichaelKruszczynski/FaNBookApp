e8228bda263a01ef626913006e11c530632f16b3
Michael Kruszczynski <michaelkruszczynski@yahoo.co.uk>
2018-04-07 17:16:44 +0100

    Initial commit

---
+++ app/src/main/java/app/mk/FaNBook/GuideActivity.java
@@ -0,0 +1,26 @@
+package app.mk.FaNBook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+/**
+ * Created by Michael Kruszczynski on 07.04.18
+ *
+ */
+
+public class GuideActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_tutorial);
+    }
+
+    public void onNextClick(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
